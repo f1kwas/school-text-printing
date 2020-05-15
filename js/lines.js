b7d18dde8ff8d581c64e2fa9e3e18f87d00d4fe5
Marcin Kwasniewski <f1kwas@gmail.com>
2020-05-15 17:54:21 +0700

    External text.txt as the text source

--- js/lines.js
+++ js/lines.js
@@ -1,14 +1,5 @@
 function main() {
 
-  var text = [
-    'Ala ma kota',
-    'Kot i Ala',
-    'Mama i Celina',
-    'Babcia',
-    'Dziadek i Babcia',
-    'Ola i Ala'
-  ];
-
   var pageWidth = 840;
   var pageHeight = 1188;
   var lineHeight = 30;
@@ -22,12 +13,7 @@ function main() {
   c.height = pageHeight;
 
   drawLines(c, lineHeight, lineColor);
-
-  var textVerticalPosition = 0;
-  for (var i = 0; i < text.length; i++) {
-    textVerticalPosition = lineHeight * 4 * (i + 1) - lineHeight;
-    printText(c, text[i], textVerticalPosition, textColor, textFont);
-  }
+  doText(fileName, c, lineHeight, textColor, textFont);
 }
 
 function drawLines(c, height, color) {
@@ -44,11 +30,27 @@ function drawLines(c, height, color) {
   }
 }
 
+function doText(fileName, c, lineHeight, color, font) {
+  var promise = fetch(fileName)
+    .then(response => response.text())
+    .then(texts => {
+      texts = texts.split('\n');
+      printLines(c, texts, lineHeight, color, font);
+    });
+}
+
+function printLines(c, text, lineHeight, color, font) {
+  var textVerticalPosition = 0;
+  for (var i = 0; i < text.length; i++) {
+    textVerticalPosition = lineHeight * 4 * (i + 1) - lineHeight;
+    printText(c, text[i], textVerticalPosition, color, font);
+  }
+}
+
 function printText(c, text, position, color, font) {
-  pageHeight = c.height;
-  pageWidth = c.width;
+  var leftMargin = 20;
   var ctx = c.getContext("2d");
   ctx.font = font;
   ctx.fillStyle = color;
-  ctx.fillText(text, 10, position);
+  ctx.fillText(text, leftMargin, position);
 }
